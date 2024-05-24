4b797450993e39ccb2d6461b6739ab6c5ee7da92
kota-yata <kota@yatagai.com>
2024-05-24 17:55:49 +0900

    update ex2

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -8,4 +8,4 @@
 mkdir -p ../certs
 
 # Generate publi - private key 
-openssl req -newkey rsa:2048 -nodes -keyout ../certs/certificate.key -x509 -out ../certs/certificate.pem -subj '/CN=Test Certificate' -addext "subjectAltName = DNS:ec2-34-212-71-222.us-west-2.compute.amazonaws.com"
+openssl req -newkey rsa:2048 -nodes -keyout ../certs/certificate.key -x509 -out ../certs/certificate.pem -subj '/CN=Test Certificate' -addext "subjectAltName = IP:34.212.71.222"
